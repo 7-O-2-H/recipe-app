e1777fc1163ae4c3ab8aaa80155f3e75da56b21a
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-17 21:56:53 -0400

    setup init seeds

---
+++ server/db/seeds/02_recipes.sql
@@ -0,0 +1,25 @@
+-- seeds/02_recipes.sql
+-- Seed Rick
+INSERT INTO recipes (id, user_id, name) VALUES (1, 1, 'pasta');
+INSERT INTO recipes (id, user_id, name) VALUES (2, 1, 'soup');
+
+-- Seed Lisa
+INSERT INTO recipes (id, user_id, name) VALUES (3, 2, 'salad');
+
+-- -- Seed Lisa
+-- INSERT INTO recipes (id, user_id, name) VALUES (9, 3, 'www.google.ca', 'iX0ff', false);
+-- INSERT INTO recipes (id, user_id, name) VALUES (10, 3, 'www.reddit.com', 'KJfCx', false);
+-- INSERT INTO recipes (id, user_id, name) VALUES (11, 3, 'www.esl-languages.com', 'DxFgg', false);
+
+-- -- Seed Simon B
+-- INSERT INTO recipes (id, user_id, name) VALUES (12, 4, 'www.vampires.com', 'CFG345', false);
+-- INSERT INTO recipes (id, user_id, name) VALUES (13, 4, 'www.wikipedia.com', 'AcGb5', true);
+-- INSERT INTO recipes (id, user_id, name) VALUES (14, 4, 'www.reddit.com', 'RRr5g', true);
+-- INSERT INTO recipes (id, user_id, name) VALUES (15, 4, 'www.google.com', 'B9Cff', false);
+-- INSERT INTO recipes (id, user_id, name) VALUES (16, 4, 'www.yahoo.ca', 'Lp09f', false);
+-- INSERT INTO recipes (id, user_id, name) VALUES (17, 4, 'www.thepiratebay.au', 'Zx099', true);
+-- INSERT INTO recipes (id, user_id, name) VALUES (18, 4, 'www.history.com', '98vCd', false);
+-- INSERT INTO recipes (id, user_id, name) VALUES (19, 4, 'www.amazon.com', 'Lj3cc', false);
+
+-- -- Seed Almight
+-- INSERT INTO recipes (id, user_id, name) VALUES (20, 5, 'www.heroacademia.jp', 'KcF43', true);
