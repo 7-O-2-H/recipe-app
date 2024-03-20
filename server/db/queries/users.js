bdb0ed159a660a115e09135e4a8138f30c8e684c
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-20 16:09:44 -0400

    add user by id and user by rec id routes, queries

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -14,4 +14,30 @@ const getAllUsers = () => {
     });
 };
 
-module.exports = { getAllUsers };
+const getUserById = (id) => {
+  return db
+    .query(`SELECT * FROM users WHERE users.id = $1`, [id])
+    .then((result) => {
+      console.log('result:', result);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log('add user error;', err.message);
+      return null;
+    });
+};
+
+const getUserByRecipeId = (id) => {
+  return db
+    .query(`SELECT * FROM users JOIN recipes ON users.id = recipes.user_id WHERE recipes.id = $1`, [id])
+    .then((result) => {
+      console.log('result:', result);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log('add user error;', err.message);
+      return null;
+    });
+};
+
+module.exports = { getAllUsers, getUserById, getUserByRecipeId };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
-const { getAllUsers } = require('../db/queries/users');
+const { getAllUsers, getUserById, getUserByRecipeId } = require('../db/queries/users');
 
 router.get('/', (req, res) => {
   getAllUsers()
+  .then(data => {
+    res.json(data);
+  })
+});
+
+router.get('/5', (req, res) => {
+  getUserById(5)
+  .then(data => {
+    res.json(data);
+  })
+});
+
+router.get('/4', (req, res) => {
+  getUserByRecipeId(4)
   .then(data => {
     res.json(data);
   })
