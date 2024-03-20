81a60f713a68b83b12881c1f9a23b31039c025a9
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-19 23:13:06 -0400

    add additional tags for testing of tag routes and queries

--- server/db/seeds/04_tags.sql
+++ server/db/seeds/04_tags.sql
@@ -1,2 +1,5 @@
 -- Seed tags
 INSERT INTO tags (id, tag) VALUES (1, 'SOUP');
+INSERT INTO tags (id, tag) VALUES (2, 'VEGAN');
+INSERT INTO tags (id, tag) VALUES (3, 'ITALIAN');
+INSERT INTO tags (id, tag) VALUES (4, 'SEAFOOD');
--- server/db/seeds/05_recipe_tags.sql
+++ server/db/seeds/05_recipe_tags.sql
@@ -1,3 +1,5 @@
 -- Seed recipe buffer
 INSERT INTO recipe_tags (id, recipe_id, tag_id) VALUES (1, 4, 1);
 INSERT INTO recipe_tags (id, recipe_id, tag_id) VALUES (2, 2, 1);
+INSERT INTO recipe_tags (id, recipe_id, tag_id) VALUES (3, 1, 3);
+INSERT INTO recipe_tags (id, recipe_id, tag_id) VALUES (4, 3, 2);
