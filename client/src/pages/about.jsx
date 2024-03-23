a4a402fbac9d29d51d8eb27953e1de47d1e002c6
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-23 03:19:48 -0400

    create about page

---
+++ client/src/pages/about.jsx
@@ -0,0 +1,15 @@
+// imports
+import NavBar from "../components/NavBar";
+
+export default function About() {
+
+  console.log('fuck you');
+  return (
+    <div>
+      
+      <NavBar />
+      <p>helooehfdskLFdsJKHF</p>
+      
+    </div>
+  );
+}
