370e6cdd74cc95cdf4d9cd9b8c83d45157b0702f
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-23 20:18:45 -0400

    initial browse page

---
+++ client/src/pages/browse.jsx
@@ -0,0 +1,17 @@
+// imports
+import NavBar from "../components/NavBar";
+import Header from '../components/Header';
+import Spacer from '../components/Spacer';
+
+export default function Browse() {
+
+  // template
+  return (
+    <div>
+      <NavBar />
+      <Spacer />
+      <Header title="Recipes" />
+      <Spacer />
+    </div>
+  );
+}
