475adf2ee274ae3b218f3c9c4a9db2e295a96208
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-26 17:53:09 -0400

    add details to recipes in db to better test css

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (id, user_name, email, password) VALUES (1, 'rickardoBoi2', 'rick.sandchez@gmail.com', 'picklerick');
+INSERT INTO users (id, user_name, email, password) VALUES (1, 'jkelly', 'jkelly85@alumni.uwo.ca', 'BCrich161!');
 INSERT INTO users (id, user_name, email, password) VALUES (2, 'saxlegend', 'lisa.simpson@gmail.com', 'ehhhhh');
 INSERT INTO users (id, user_name, email, password) VALUES (3, 'ImNotZelda', 'link@yahoo.com', 'hyrule');
 INSERT INTO users (id, user_name, email, password) VALUES (4, 'xBLOODlustx', 'drac@mail.ca', 'dracula');
--- server/db/seeds/03_recipes.sql
+++ server/db/seeds/03_recipes.sql
@@ -1,12 +1,15 @@
 -- seeds/02_recipes.sql
--- Seed Rick
-INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (1, 1, 'pasta', 10, 1, 4, 'pasta');
-INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (2, 1, 'soup', 10, 1, 4, 'soup');
-
--- Seed Lisa
-INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (3, 2, 'salad', 10, 1, 4, 'salad');
-
--- Seed Vavo
+INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (1, 1, 'Pasta', 10, 1, 4, 'Some kind of pasta!');
+INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (2, 2, 'Cream of Mushroom Soup', 10, 1, 4, 'Like magic, it''s creamy without a lot of cream.');
+INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (
+  3,
+  1,
+  'Marry Me Chicken', 
+  1, 
+  2, 
+  4, 
+  'A flavourful tomato-based chicken dish with fresh basil.'
+);
 INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (
   4, 
   5, 
