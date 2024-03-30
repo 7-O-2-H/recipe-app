3b2c931f4fb2064da4daa8e67de781c6d6bc620c
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-30 16:38:33 -0400

    remove test code from rec list

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -7,22 +7,12 @@ import RecipeListItem from "./RecipeListItem";
 
 export default function RecipeList() {
 
-  //const [allRecipes, setAllRecipes] = useState([]);
-
-  // useEffect(() => {
-  //   getAllRecipes()
-  //   .then((data) => {
-  //     setAllRecipes(data['data']);
-  //   })
-  // }, []);
-
   const { allRecipes } = useAppData();
-  // console.log(allRecipes[0].description);
 
   const recipesArray = allRecipes.map(recipe => (
       <RecipeListItem
         key={recipe.id}
-        // submitted={recipe.user_id}
+        submitted={recipe.user_id}
         name={recipe.recipe}
         time={recipe.time}
         unit={recipe.measurement}
@@ -36,8 +26,6 @@ export default function RecipeList() {
   console.log(allRecipes, recipesArray);
   return (
     <div className="recipe-list">
-      {/* <RecipeListItem key={} name={'Some New Recipe'} time={30} unit={'minutes'} servingSize={4} description={'It\s a cool new recipe that will totally get you laid!'} /> */}
-      {/* <RecipeListItem key={recipe.id} name={recipe.recipe} description={recipe.description} /> */}
       {recipesArray}
     </div>
   );
