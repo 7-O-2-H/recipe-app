01255297a1ccc89cafd0008a3bd1c8b2ad3ac43c
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-03-31 15:13:03 -0400

    add zuppa toscana rec

--- server/db/seeds/03_recipes.sql
+++ server/db/seeds/03_recipes.sql
@@ -1,15 +1,26 @@
 -- seeds/02_recipes.sql
-INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (1, 1, 'Pasta', 10, 1, 4, 'Some kind of pasta!');
-INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (2, 2, 'Cream of Mushroom Soup', 10, 1, 4, 'Like magic, it''s creamy without a lot of cream.');
+-- seed Josh
 INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (
-  3,
   1,
-  'Marry Me Chicken', 
+  1,
+  'Chicken in Tomato Sauce', 
+  25, 
   1, 
-  2, 
   4, 
   'A flavourful tomato-based chicken dish with fresh basil.'
 );
+-- seed Gary
+INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (
+  2,
+  1,
+  'Zuppa Toscana',
+  35,
+  1,
+  4,
+  'A creamy and hearty Italian soup.'
+);
+INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (3, 2, 'Cream of Mushroom Soup', 10, 1, 4, 'Like magic, it''s creamy without a lot of cream.');
+-- seed Vavo
 INSERT INTO recipes (id, user_id, recipe, time, measurement_id, serves, description) VALUES (
   4, 
   5, 
