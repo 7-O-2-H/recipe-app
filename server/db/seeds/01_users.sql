4c0fec028c37620f0f085985b888cf328fef9ce2
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-10 15:55:02 -0400

    update users

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -2,6 +2,6 @@
 -- users seeds
 INSERT INTO users (id, user_name, email, password) VALUES (1, 'jkelly', 'jkelly85@alumni.uwo.ca', 'BCrich161!');
 INSERT INTO users (id, user_name, email, password) VALUES (2, 'gMONEY', 'GK@gmail.com', 'Detroit7');
-INSERT INTO users (id, user_name, email, password) VALUES (3, 'ImNotZelda', 'link@yahoo.com', 'hyrule');
+INSERT INTO users (id, user_name, email, password) VALUES (3, 'lizard', 'lizard@yahoo.com', '200265');
 INSERT INTO users (id, user_name, email, password) VALUES (4, 'xBLOODlustx', 'drac@mail.ca', 'dracula');
 INSERT INTO users (id, user_name, email, password) VALUES (5, 'VAV0', 'i_med@mail.ca', 'manuel');
