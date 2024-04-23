cf437af00410d99af880bdda03c851b70961c7f4
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-23 14:19:56 -0400

    conversion of decimal measurments

--- client/src/components/FullRecipe.jsx
+++ client/src/components/FullRecipe.jsx
@@ -2,11 +2,23 @@ export default function FullRecipe (props) {
 
   const { recipe, ingredients, steps } = props;
 
+
   // create array of ingredients with measurement and quantity
   const ingredientsArray = [];
   for (const ing of ingredients) {
     let str = '';
     if (ing['quantity'] > 1) {
+      if (ing['quantity'] % 1 === 0) {
+        str += `${ing['quantity']} ${ing['measurement']}s of ${ing['ingredient']}`; 
+      } else {
+        const wholeNum = ing['quantity'] - (ing['quantity'] % 1);
+        const decimal = ing['quantity'] % 1;
+        if (decimal === 0.66) {
+          str += `${wholeNum} 2/3 ${ing['measurement']}s of ${ing['ingredient']}`;
+        } else if (decimal === 0.33) {
+          str += `${wholeNum} 1/3 ${ing['measurement']}s of ${ing['ingredient']}`;
+        }
+      }
       str += `${ing['quantity']} ${ing['measurement']}s of ${ing['ingredient']}`; 
     } else {
       str += `${ing['quantity']} ${ing['measurement']} of ${ing['ingredient']}`;
