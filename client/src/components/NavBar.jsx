870ac1393e6c76469596ea96302531ed69317d75
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-24 11:58:55 -0400

    add logout functions

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,27 @@
 // Imports
 import "../styles/NavBar.css";
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
   
+  // set router
+  const router = useRouter();
+
+  // set default login status to false
   let loggedIn = false;
 
   // check if localStorage is defined
   if (typeof window!== 'undefined') {
     // get logged in status
     loggedIn = JSON.parse(localStorage.getItem("loggedIn"));
+  };
+
+  //handle logout button click
+  const handleLogout = () => {
+    loggedIn = false;
+    localStorage.setItem("loggedIn", false);
+    router.push('/');
   };
 
   // Template
@@ -36,7 +48,7 @@ export default function NavBar() {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <h1>Favourites</h1>
+            {/* <h1>Favourites</h1> */}
           </li>
           <li>
             {/* <h1>My Recipes</h1> */}
@@ -45,7 +57,7 @@ export default function NavBar() {
             <Link href="/browse">Browse</Link>
           </li>
           <li>
-            {/* <h1>Logout</h1> */}
+            <button className="logoutButton" onClick={handleLogout}>Logout</button>
           </li>
         </ul> 
       )}
