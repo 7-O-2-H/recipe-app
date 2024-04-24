bfb8ee04a64b228093588669c468a81887f53a35
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-24 11:45:49 -0400

    render nav links dependant of whether client is loggedin

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,26 +4,51 @@ import Link from "next/link";
 
 export default function NavBar() {
   
-  const loggedIn = JSON.parse(localStorage.getItem("loggedIn"));
+  let loggedIn = false;
+
+  // check if localStorage is defined
+  if (typeof window!== 'undefined') {
+    // get logged in status
+    loggedIn = JSON.parse(localStorage.getItem("loggedIn"));
+  };
 
   // Template
   return (
     <div className="navbar">
-
-      <ul className="links">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/browse">Browse</Link>
-        </li>
-        <li>
-          <Link href="/login">Login</Link>
-        </li>
-      </ul>
+      {!loggedIn ? (
+        <ul className="links">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/browse">Browse</Link>
+          </li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+        </ul> 
+      ) : (
+        <ul className="links">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <h1>Favourites</h1>
+          </li>
+          <li>
+            {/* <h1>My Recipes</h1> */}
+          </li>
+          <li>
+            <Link href="/browse">Browse</Link>
+          </li>
+          <li>
+            {/* <h1>Logout</h1> */}
+          </li>
+        </ul> 
+      )}
     </div>
   );
 }
