679176aba1381b665d08b49ab7a6684cff566d46
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-24 12:41:26 -0400

    add random recipe to home page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -5,12 +5,26 @@ import Header from '../components/Header';
 import Spacer from '../components/Spacer';
 import FullRecipe from '../components/FullRecipe';
 import useAppData from '../hooks/useAppData';
+import { useRecipe } from '../hooks/useRecipe';
 
 export default function Home() {
 
+  // retreive all recipes
   const { allRecipes } = useAppData();
 
-  console.log("all recs:", allRecipes, "\nnumber of recipes: ", allRecipes.length)
+  // create random index
+  const recipeIndex = Math.floor(Math.random() * allRecipes.length);
+
+  // use random number to select random recipe
+  const randomRecipe = allRecipes[recipeIndex];
+
+  // get all recipe data
+  const { currentRecipe, currentIngredients, currentSteps } = useRecipe(randomRecipe?.id);
+
+  //loading state 
+  if (!randomRecipe || !currentRecipe || !currentIngredients || !currentSteps) {
+    return <div>Loading...</div>;
+  }
   
   // template
   return (
@@ -19,7 +33,11 @@ export default function Home() {
     <Spacer />
     <Header title="Add to Taste" />
     <Spacer />
-    <FullRecipe />
+    <FullRecipe 
+      recipe={currentRecipe} 
+      ingredients={currentIngredients}
+      steps={currentSteps} 
+    />
    </div>
   );
 }
