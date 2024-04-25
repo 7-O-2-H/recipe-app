0ab02964fcf486b8d3e74e49833168c282a2aa18
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-25 18:46:09 -0400

    add addFavourite route: favourites/add

--- server/routes/favourites.js
+++ server/routes/favourites.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const { getFavouritesByUserId, getAllFavourites } = require('../db/queries/favourites');
+const { getFavouritesByUserId, getAllFavourites, addFavourite } = require('../db/queries/favourites');
 
 router.get('/:id', (req, res) => {
   const userId = req.params.id;
   getFavouritesByUserId(userId)
   .then(data => {
-    console.log(data);
     res.json(data);
   })
 });
@@ -13,7 +12,18 @@ router.get('/:id', (req, res) => {
 router.get('/', (req, res) => {
   getAllFavourites()
   .then(data => {
-    console.log(data);
+    res.json(data);
+  })
+});
+
+router.post('/add', (req, res) => {
+
+  // req userId and recipeId
+  const userId = req.params.userId;
+  const recipeId = req.params.recipeId;
+
+  addFavourite(userId, recipeId)
+  .then(data => {
     res.json(data);
   })
 });
