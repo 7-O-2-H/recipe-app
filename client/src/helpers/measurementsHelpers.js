6f1194d52997803d34c131fe5ea95cf1a3bcf0fd
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-03 17:54:57 -0400

    add measurements to useAppData hook

--- client/src/helpers/measurementsHelpers.js
+++ client/src/helpers/measurementsHelpers.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // get all tags
-export function getAllmeasurements() {
+export function getAllMeasurements() {
 
   return axios.get('http://localhost:8080/measurements')
   .then((data) => {
--- client/src/pages/addRecipe.jsx
+++ client/src/pages/addRecipe.jsx
@@ -4,15 +4,19 @@ import NavBar from '../components/NavBar';
 import Header from '../components/Header';
 import Spacer from '../components/Spacer';
 import AddForm from '../components/addRecipe/AddForm'
+import useAppData from '../hooks/useAppData';
 
 export default function AddRecipe() {
 
+  const { allMeasurements } = useAppData();
+  console.log(allMeasurements);
+  
   // template
   return (
    <div>
     <NavBar />
     <Spacer />
-    <Header title="Add Recips" />
+    <Header title="Add Recipe" />
     <Spacer />
     <AddForm />
    </div>
