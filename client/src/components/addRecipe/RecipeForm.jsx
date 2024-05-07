6256c723e22bbe55282e3fe7e6025a0e11361f6b
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-06 23:47:32 -0400

    attempting to add recipe with a name that's already in db results in error

--- client/src/components/addRecipe/RecipeForm.jsx
+++ client/src/components/addRecipe/RecipeForm.jsx
@@ -1,5 +1,9 @@
 // imports
 import { useState } from "react";
+import useAppData from "../../hooks/useAppData";
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function RecipeForm () {
 
@@ -8,43 +12,62 @@ export default function RecipeForm () {
   const [serves, setServes] = useState(undefined);
   const [description, setDescription] = useState('');
 
+  const { allRecipes } = useAppData();
+
+  if ( !allRecipes ) {
+    return <div>Loading...</div>
+  };
+
+  const handleRecipedSubmit = (event) => {
+    event.preventDefault();
+
+    const existingRecipe = allRecipes.find(rec => rec.recipe.toLowerCase() === (recipe.toLowerCase()));
+    if (existingRecipe) {
+      toast.error("There is already a recipe with this name in our database.");
+    };
+    return;
+  }
+
   return (
-    <form className="recipe-form" >
-      <input
-        id="recipe"
-        type="text"
-        className="input-field"
-        placeholder="recipe name"
-        value={recipe}
-        onChange={(event) => setRecipe(event.target.value)}
-      />
-      <input
-        id="time"
-        type="number"
-        className="input-field"
-        placeholder="time to cook in minutes"
-        value={time}
-        onChange={(event) => setTime(event.target.value)}
-      />
-      <input
-        id="serves"
-        type="number"
-        className="input-field"
-        placeholder="how many servings is this dish"
-        value={serves}
-        onChange={(event) => setServes(event.target.value)}
-      />
-      <input
-        id="description"
-        type="text"
-        className="input-field"
-        placeholder="description"
-        value={description}
-        onChange={(event) => setDescription(event.target.value)}
-      />
-      <button type="submit" className="submit-btn">
-        ADD RECIPE DETAILS
-      </button>
-    </form>
+    <div>
+      <ToastContainer />
+      <form className="recipe-form" onSubmit={handleRecipedSubmit}>
+        <input
+          id="recipe"
+          type="text"
+          className="input-field"
+          placeholder="recipe name"
+          value={recipe}
+          onChange={(event) => setRecipe(event.target.value)}
+        />
+        <input
+          id="time"
+          type="number"
+          className="input-field"
+          placeholder="time to cook in minutes"
+          value={time}
+          onChange={(event) => setTime(event.target.value)}
+        />
+        <input
+          id="serves"
+          type="number"
+          className="input-field"
+          placeholder="how many servings is this dish"
+          value={serves}
+          onChange={(event) => setServes(event.target.value)}
+        />
+        <input
+          id="description"
+          type="text"
+          className="input-field"
+          placeholder="description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          ADD RECIPE DETAILS
+        </button>
+      </form>
+    </div>
   );
 };
