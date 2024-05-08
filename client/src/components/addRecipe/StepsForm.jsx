cf86b08578652d709876cecf17dffec4611e7761
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-08 19:07:16 -0400

    add toast to steps form

--- client/src/components/addRecipe/StepsForm.jsx
+++ client/src/components/addRecipe/StepsForm.jsx
@@ -1,5 +1,8 @@
 // imports
 import { useState } from "react";
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import useAppData from "../../hooks/useAppData";
 
 export default function StepsForm (props) {
