b8e793d6df0edcac73ea6128ebf9afa50f96c563
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-08 19:13:19 -0400

    build steps form in component

--- client/src/components/addRecipe/StepsForm.jsx
+++ client/src/components/addRecipe/StepsForm.jsx
@@ -7,7 +7,34 @@ import useAppData from "../../hooks/useAppData";
 
 export default function StepsForm (props) {
 
+  //  set initial states
+  const [stepName, setStepName] = useState('');
+  const [instruction, setInstruction] = useState('');
+  
   return (
-    <div></div>
+    <div>
+      <ToastContainer />
+      <form className="steps-form" onSubmit={handleRecipeSubmit}>
+        <input
+          id="step-name"
+          type="text"
+          className="input-field"
+          placeholder="step or prep"
+          value={recipe}
+          onChange={(event) => setStepName(event.target.value)}
+        />
+        <input
+          id="instruction"
+          type="number"
+          className="input-field"
+          placeholder="instruction"
+          value={instruction}
+          onChange={(event) => setInstruction(event.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          ADD RECIPE DETAILS
+        </button>
+      </form>
+    </div>
   )
-  };
+};
