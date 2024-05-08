873e74cb47d88df8f75a27adfdae9c7d4333b28e
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-08 19:06:20 -0400

    remove unused code from ingredients routes

--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllIngredients, getIngredientByName, getIngredientsByRecipeId } = require('../db/queries/ingredients');
+const { getAllIngredients, getIngredientByName, getIngredientsByRecipeId, addRecipeIngredient, addIngredient} = require('../db/queries/ingredients');
 
 // ingredients
 router.get('/', (req, res) => {
@@ -18,7 +18,9 @@ router.get('/Celery', (req, res) => {
 
 // add
 router.post('/add', (req, res) => {
+
   const ingredientData = req.body.ingredientData;
+
   addIngredient(ingredientData)
   .then(data => {
     res.json(data);
@@ -26,19 +28,19 @@ router.post('/add', (req, res) => {
 });
 
 // `/#{id}`
-router.get('/1', (req, res) => {
-  getIngredientsByRecipeId(1)
-  .then(data => {
-    res.json(data);
-  })
-});
+// router.get('/1', (req, res) => {
+//   getIngredientsByRecipeId(1)
+//   .then(data => {
+//     res.json(data);
+//   })
+// });
 
-router.get('/2', (req, res) => {
-  getIngredientsByRecipeId(2)
-  .then(data => {
-    res.json(data);
-  })
-});
+// router.get('/2', (req, res) => {
+//   getIngredientsByRecipeId(2)
+//   .then(data => {
+//     res.json(data);
+//   })
+// });
 
 
 module.exports = router;
