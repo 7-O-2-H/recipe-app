7f0e3a7f7162d0a64570dafa145aca649ddd1284
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-09 00:27:51 -0400

    update add recipe route to call psql insert query

--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -20,27 +20,17 @@ router.get('/Celery', (req, res) => {
 router.post('/add', (req, res) => {
 
   const ingredientData = req.body.ingredientData;
-  console.log(ingredientData);
+  // console.log(ingredientData, "type of existing: ", typeof ingredientData.existingIngredient);
+  if (ingredientData.existingIngredient) {
+    addRecipeIngredient(ingredientData)
+    .then(data => {
+      res.json(data);
+    });
+  }
   // addIngredient(ingredientData)
   // .then(data => {
   //   res.json(data);
   // });
 });
-
-// `/#{id}`
-// router.get('/1', (req, res) => {
-//   getIngredientsByRecipeId(1)
-//   .then(data => {
-//     res.json(data);
-//   })
-// });
-
-// router.get('/2', (req, res) => {
-//   getIngredientsByRecipeId(2)
-//   .then(data => {
-//     res.json(data);
-//   })
-// });
-
 
 module.exports = router;
