bb91508f7615aaada966a11e052c60a2dd1f38e6
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-18 16:18:36 -0400

    skipping tags redirects to full recipe

--- client/src/components/addRecipe/TagsPrompt.jsx
+++ client/src/components/addRecipe/TagsPrompt.jsx
@@ -1,14 +1,22 @@
 import './styles/tagsPrompt.css';
+import { useRouter } from 'next/router';
+
 export default function TagsPrompt(props) {
 
+  const router = useRouter();
   const { recipeId } = props;
+
+  // handlers
+  const handleSkipTags = (e) => {
+    router.push(`/recipes/${recipeId}`);
+  };
 
   return (
     <div className="tags=prompt">
       <h2>Add Tags?</h2>
       <p>Adding tags makes it easier for others to find your recipes.</p>
       <div className="tags-prompt-buttons">
-        <button>SKIP</button>
+        <button onClick={handleSkipTags}>SKIP</button>
         <button>TAGS</button>
       </div>
     </div>
