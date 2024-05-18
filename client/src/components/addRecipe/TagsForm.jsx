13b6cd06b2c514ec38e37f6b4dbf6897b5a7d055
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-18 15:50:18 -0400

    add form input dwtails for tagsForm

--- client/src/components/addRecipe/TagsForm.jsx
+++ client/src/components/addRecipe/TagsForm.jsx
@@ -8,8 +8,29 @@ import { addStep } from "../../helpers/stepsHelpers";
 
 export default function TagsForm (props) {
 
+  const { recipeId } = props;
+
+  const [tagContainer, setTagContainer] = useState('');
+
+  // handle tag input
+  const handleTagChange = (e) => {
+    const tag = onput.target.value;
+    setTagContainer(tag);
+  };
+
   // template
   return (
-    <div></div>
+    <div>
+      <form className="tags-form" >
+        <input
+          id="tag"
+          type="text"
+          className="tag"
+          placeholder=" Search for tag"
+          value={tagContainer}
+          onChange={handleTagChange}
+        />
+      </form>
+    </div>
   );
 };
