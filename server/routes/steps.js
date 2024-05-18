2f9274b1166eec4cb68a1c4d99738cfca819d989
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-17 22:27:20 -0400

    add steps works with database

--- server/routes/steps.js
+++ server/routes/steps.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { addStep } = require('../db/queries/steps');
 
 // POST
 router.post('/add', (req, res) => {
@@ -6,8 +7,7 @@ router.post('/add', (req, res) => {
   // req recipe data
   const stepData = req.body.stepData;
 
-  console.log(stepData);
-
+  addStep(stepData);
   // addRecipe(recipeData)
   // .then(data => {
   //   res.json(data);
