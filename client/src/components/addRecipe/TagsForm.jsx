4dcce82e9678e1dad55fb603b1841777e1ee1d3a
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-18 16:59:56 -0400

    tags are retreived in tagForm

--- client/src/components/addRecipe/TagsForm.jsx
+++ client/src/components/addRecipe/TagsForm.jsx
@@ -3,20 +3,25 @@ import { useEffect, useState } from "react";
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import useAppData from "../../hooks/useAppData";
+import { useAppData } from "../../hooks/useAppData";
 import { addStep } from "../../helpers/stepsHelpers";
 
 export default function TagsForm (props) {
 
+  // rectreive recipe ID from props
   const { recipeId } = props;
 
+  // set initial states
   const [tagContainer, setTagContainer] = useState('');
+
+  // retreive tags from db
+  const { allTags } = useAppData();
 
   // handle tag input
   const handleTagChange = (e) => {
-    const tag = onput.target.value;
-    setTagContainer(tag);
-    console.log(tagContainer);
+    const inputValue = e.target.value;
+    setTagContainer(inputValue);
+    console.log(allTags);
   };
 
   // template
