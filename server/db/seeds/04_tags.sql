40e88931597b381c5c7f953046a55db577c1cae8
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-22 15:33:46 -0400

    make tags singular so they can be easily parsed by suggester

--- server/db/seeds/04_tags.sql
+++ server/db/seeds/04_tags.sql
@@ -1,5 +1,5 @@
 -- Seed tags
-INSERT INTO tags (tag) VALUES ('soups');
+INSERT INTO tags (tag) VALUES ('soup');
 INSERT INTO tags (tag) VALUES ('vegan');
 INSERT INTO tags (tag) VALUES ('Italian');
 INSERT INTO tags (tag) VALUES ('seafood');
@@ -15,7 +15,7 @@ INSERT INTO tags (tag) VALUES ('serves 4-6');
 INSERT INTO tags (tag) VALUES ('healthy');
 INSERT INTO tags (tag) VALUES ('asian');
 INSERT INTO tags (tag) VALUES ('mexican');
-INSERT INTO tags (tag) VALUES ('desserts');
+INSERT INTO tags (tag) VALUES ('dessert');
 INSERT INTO tags (tag) VALUES ('breakfast');
 INSERT INTO tags (tag) VALUES ('lunch');
 INSERT INTO tags (tag) VALUES ('dinner');
