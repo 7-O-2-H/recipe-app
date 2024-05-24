35cc5bc3c7ce78a38935eec19f2ce143b1154927
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-05-24 15:52:36 -0400

    give edit button functionality

--- client/src/components/editRecipe/EditForm.jsx
+++ client/src/components/editRecipe/EditForm.jsx
@@ -10,8 +10,6 @@ export default function EditForm() {
   const router = useRouter();
   const { recipe, ingredients, steps } = router.query;
 
-  console.log(ingredients);
-
   const currentRecipe = JSON.parse(recipe);
   const currentIngredients = JSON.parse(ingredients);
   const currentSteps = JSON.parse(steps);
@@ -51,7 +49,6 @@ export default function EditForm() {
       <div className="edit-section">
         <h4 id="edit-category">INGREDIENTS</h4>
         <div className="edit-content">{ingredientArray}</div>
-        {/* <button className="edit-button">EDIT</button> */}
       </div>
       <div className="edit-section">
         <h4 id="edit-category">INSTRUCTIONS</h4>
--- client/src/components/editRecipe/EditableIngredient.jsx
+++ client/src/components/editRecipe/EditableIngredient.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 export default function EditableIngredient(props) {
   
   const { ingredient } = props;
+
+  const [editable, setEditable] = useState(false);
+
+  // handle edit condition
+  const editIngredient = (e) => {
+    e.preventDefault();
+    setEditable(true);
+  };
+
   //template
   return (
     <div className="ingredients-container">
@@ -9,7 +20,7 @@ export default function EditableIngredient(props) {
         {ingredient}
         <br></br>
         <br></br>
-        <button className="edit-button">EDIT</button>
+        <button className="edit-button" onClick={editIngredient}>EDIT</button>
       </div>
     </div>
   );
