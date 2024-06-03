a2f2962f57513d53bd7e700d8062c9af765dc26c
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-06-03 17:16:21 -0400

    set initial state to props in edit steps form

--- client/src/components/editRecipe/EditSteps.jsx
+++ client/src/components/editRecipe/EditSteps.jsx
@@ -188,6 +188,11 @@
 export default function EditSteps(props) {
   
   const { step_number, step_name, instruction } = props;
+
+  // set states
+  const [stepNumber, setStepNumber] = useState(step_number);
+  const [stepName, setStepName] = useState(step_name);
+  const [instructionContainer, setInstructionContainer] = useState(instruction);
   //template
   return (
     <div className="steps-container">
