d704f5dd22ae2b673bede2719886c4ac5550497f
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-06-06 12:15:43 -0400

    import delete step into edit form

--- client/src/components/editRecipe/EditSteps.jsx
+++ client/src/components/editRecipe/EditSteps.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { deleteStepById } from "../../helpers/stepsHelpers";
 
 export default function EditSteps(props) {
   
