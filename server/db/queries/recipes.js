f653b40ff2015ec7adcef47179fb9bb91d66faba
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-06-11 18:26:26 -0400

    fix steps query to order by step number

--- server/db/queries/recipes.js
+++ server/db/queries/recipes.js
@@ -106,7 +106,7 @@ const getIngredientsByRecipeId = (id) => {
 
 // steps
 const getStepsByRecipeId = (id) => {
-  return db.query("SELECT * FROM steps WHERE recipe_id = $1", [id]).then(data => {
+  return db.query("SELECT * FROM steps WHERE recipe_id = $1 ORDER BY step_number;", [id]).then(data => {
     return data.rows;
   })
   .catch((err) => {
