e689f40953a0d7573b28aa6cfc8324ce191723c9
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-06-20 14:35:44 -0400

    create useRecipeWithRefresh to use in refreshing edit form

--- client/src/hooks/useRecipe.jsx
+++ client/src/hooks/useRecipe.jsx
@@ -30,4 +30,33 @@ export function useRecipe(recipeId) {
 
   return { currentRecipe, currentIngredients, currentSteps, currentTags };
 
+};
+
+export function useRecipeWithRefresh(recipeId, refreshData) {
+  const [currentRecipe, setCurrentRecipe] = useState(null);
+  const [currentIngredients, setCurrentIngredients] = useState(null);
+  const [currentSteps, setCurrentSteps] = useState(null);
+  const [currentTags, setCurrentTags] = useState(null);
+  
+  useEffect(() => {
+
+    if (recipeId) {
+      Promise.all([
+        getRecipeByRecipeId(recipeId),
+        getIngredientsByRecipeId(recipeId),
+        getStepsByRecipeId(recipeId),
+        getTagsByRecipeId(recipeId),
+      ])
+      .then((all) => {
+        setCurrentRecipe(all[0]['data'][0]);
+        setCurrentIngredients(all[1]['data']);
+        setCurrentSteps(all[2]['data']);
+        setCurrentTags(all[3]['data']);
+      })
+    };
+    
+  }, [recipeId, refreshData]);
+
+  return { currentRecipe, currentIngredients, currentSteps, currentTags };
+
 }
