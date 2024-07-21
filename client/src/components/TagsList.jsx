8cfe488a8bfc07cd8187164e73e7ece1cd371ee4
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-07-21 19:14:23 -0400

    refactor tags list to use function pased as prop instead of local storage, pass to tags dropdown

--- client/src/components/TagsList.jsx
+++ client/src/components/TagsList.jsx
@@ -2,9 +2,8 @@
 import { useState } from "react";
 import { useAppData } from "../hooks/useAppData";
 import TagsDropdown from "../components/TagsDropdown";
-import { useRouter } from "next/router";
 
-export default function TagsList() {
+export default function TagsList({ handleSelectTag }) {
 
   const router = useRouter();
 
@@ -15,18 +14,21 @@ export default function TagsList() {
   const { allTags } = useAppData();
   
   const handleClear = () => {
-    localStorage.setItem('tag', '');
-    router.push('/browse');
+    setSelectedOption;('');
   };
 
-  const handleSelect = (selectedOption) => {
-    localStorage.setItem('tag', selectedOption);
-    router.push('/browse');
-  };
 
   return (
     <div>
-      <TagsDropdown tags={allTags} selectOption={selectedOption} onSelect={handleSelect} clear={handleClear} />
+      <TagsDropdown
+        tags={allTags}
+        selectOption={selectedOption}
+        onSelect={(value) => {
+          setSelectedOption(value);
+          handleSelectTag(value);
+          }}
+          clear={handleClear}
+        />
     </div>
   );
 }
