62ed8358cf437219d4eba98dbf781207fff52fdf
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-07-21 18:34:54 -0400

    refactor browse page - move select ingredient handle and pass as prop to browse options, pass selected ingredient to rec list

--- client/src/pages/browse.jsx
+++ client/src/pages/browse.jsx
@@ -1,11 +1,26 @@
 // imports
+//styles
 import "../styles/browse.css";
+// react
 import { useState } from "react";
+import { useRouter } from "next/router";
+// components
 import NavBar from "../components/NavBar";
 import Header from '../components/Header';
 import Spacer from '../components/Spacer';
 import RecipeList from "../components/RecipeList";
 import BrowseOptions from "../components/BrowseOptions";
+
+const router = useRouter();
+
+const [ingredient, setIngredient] = useState('');
+
+// handlers to be used to set filter data, update state vars and refresh rec list
+const handleSelectIngredient = (selectedOption) => {
+
+  setIngredient(selectedOption);
+  router.push('/browse');
+};
 
 export default function Browse() {
 
@@ -16,9 +31,13 @@ export default function Browse() {
       <Spacer />
       <Header title="Recipes" />
       <Spacer />
-      <BrowseOptions />
+      <BrowseOptions
+        handleSelect={handleSelectIngredient}   
+      />
       <div className="browse-body" >
-        <RecipeList />
+        <RecipeList
+          ingredient={ingredient}
+        />
       </div>
     </div>
   );
