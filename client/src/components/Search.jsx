e79ed2c0a894eefc5adb2f6fabf32ea296e5b2ad
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-07-23 16:39:16 -0400

    update search component to use the passed handle search function, remove useRouter - no longer used

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,32 +1,19 @@
 // imoprts
 import { useState } from 'react';
-import { useRouter } from 'next/router';
 
-function Search() {
+export default function Search({ handleSearch }) {
 
-  const router = useRouter();
-
-  const [query, setQuery] = useState('');
-
-  const handleSearch = () => {
-    // Perform search action here
-    // You can send the query to your backend server or search locally
-    localStorage.setItem('query', query);
-    router.push('/browse');
-    // onSearch(query);
-  };
+  const [searchQuery, setSearchQuery] = useState('');
 
   return (
     <div>
       <input
         type="text"
-        value={query}
+        value={searchQuery}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="SEARCH"
       />
       <button onClick={handleSearch} className='search-tools-button'>SEARCH</button>
     </div>
   );
-}
-
-export default Search;
+};
